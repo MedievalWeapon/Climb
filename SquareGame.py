fd832b1f7d3ab9c0db60f23f3b50f4de2ba98f6f
notverywise <nickyboyindawindow@gmail.com>
2021-05-19 17:06:14 +0100

    boom banger banger

--- SquareGame.py
+++ SquareGame.py
@@ -3,7 +3,7 @@ import keyboard
 
 root = Tk()
 root.title('SquareGame')
-roomSize = 500
+roomSize = 700
 
 class Box:
 	def __init__(self):
@@ -11,10 +11,11 @@ class Box:
 		self.y = 15
 		self.dy = 0
 		self.g = -0.1
-		self.jump = 5
+		self.jump = 2
 		self.step = 2
 		self.width = 10
 		self.height = 10
+		self.onGround = False
 		self.box = room.create_rectangle(10, 10, 10 + self.width, 10 + self.height, fill='blue')
 		room.after(10, self.loop)
 		
@@ -22,22 +23,36 @@ class Box:
 		xStep = (keyboard.is_pressed('right') - keyboard.is_pressed('left')) * self.step
 		##yStep = (keyboard.is_pressed('down') - keyboard.is_pressed('up')) * self.step
 		self.dy -= self.g
-		if(keyboard.is_pressed('up')):
+		if(keyboard.is_pressed('up') and self.onGround):
 			self.dy = -self.jump
+		self.onGround = False
 		self.x += xStep
 		self.y += self.dy
 		room.move(self.box, xStep, self.dy)
 		self.bounding()
 		room.after(10, self.loop)
-			
+		
 	def bounding(self):
+		self.boundingFloor()
+		self.boundingSides()
+			
+	def boundingFloor(self):
 		if self.y + (self.height / 2) > roomSize:
 			yStep = (self.y + (self.height / 2)) - roomSize
 			self.y -= yStep
-			print(self.y)
 			self.dy = 0
 			room.move(self.box, 0, -yStep)
+			self.onGround = True
 			
+	def boundingSides(self):
+		if self.x + (self.width / 2) > roomSize:
+			xStep = (self.x + (self.width / 2)) - roomSize
+			self.x -= xStep
+			room.move(self.box, -xStep, 0)
+		if self.x - (self.width / 2) < 0:
+			xStep = (self.x - (self.width / 2)) 
+			self.x -= xStep
+			room.move(self.box, -xStep, 0)
 
 
 room = Canvas(root, height=roomSize, width=roomSize)
